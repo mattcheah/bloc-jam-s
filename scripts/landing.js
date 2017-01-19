a3587094290fe32ae305bd50e83a8493444ff096
Matt C <matt.cheah@gmail.com>
2017-01-19 13:24:34 -0800

    change landing.js to function revealPoint

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,20 @@
 var animatePoints = function() {
                     
-    var points = document.getElementsByClassName('point');
+    var pointNodes = document.getElementsByClassName("point");
+    
+    for (var i = 0; i < pointNodes.length; i++) {
+        
+        console.log("hi "+i);
+        setTimeout(revealPoint(i), 250*i);
+    }
+    
+    function revealPoint() {
+        pointNodes[i].style.opacity = 1;
+        pointNodes[i].style.transform = "scaleX(1) translateY(0)";
+        pointNodes[i].style.msTransform = "scaleX(1) translateY(0)";
+        pointNodes[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
 
-    var revealFirstPoint= function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    var revealSecondPoint= function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    var revealThirdPoint= function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
 };
-            
-            
+
+animatePoints();
