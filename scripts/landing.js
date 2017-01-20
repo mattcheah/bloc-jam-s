8aac76ed63e960968f07e03015ad0846c8b75ce9
Matt C <matt.cheah@gmail.com>
2017-01-19 16:11:58 -0800

    add functionality for points to appear on scroll

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,20 +1,39 @@
-var revealPoint = function(pointNodes) {
-                    
-    var pointNodes = document.getElementsByClassName("point");
+var pointNodes = document.getElementsByClassName("point");
+var sellingPoints = document.getElementsByClassName('selling-points')[0];
+var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+var animatePoints = function(pointNodes) {
     
-    for (var i = 0; i < pointNodes.length; i++) {
-        
-        console.log("hi "+i);
-        setTimeout(revealPoint(i), 250*i);
-    }
-    
-    function revealPoint() {
+    var revealPoint = function(i) {
         pointNodes[i].style.opacity = 1;
         pointNodes[i].style.transform = "scaleX(1) translateY(0)";
         pointNodes[i].style.msTransform = "scaleX(1) translateY(0)";
         pointNodes[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
+    
+    for (var i = 0; i < pointNodes.length; i++) {
+        
+        //console.log("hi "+i);
+        setTimeout(revealPoint(i), 250*i);
+    }
+    
+    
 
 };
 
-animatePoints();
+window.onload = function() {
+    
+    if(window.innerHeight > 950) {
+         animatePoints(pointNodes);
+    }
+
+    window.addEventListener('scroll', function(event) {
+        if(document.documentElement.scrollTop || document.body.scrollTop > scrollDistance) {
+             animatePoints(pointNodes);
+        }
+    });
+    
+    
+}
+
+
