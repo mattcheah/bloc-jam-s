9bc13275198f18af4c7c93cfda52bf2f76db7a96
Matt C <matt.cheah@gmail.com>
2017-01-25 11:26:52 -0800

    add jquery and change vanilla scripts to jquery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,33 @@
-var pointNodes = document.getElementsByClassName("point");
-var sellingPoints = document.getElementsByClassName('selling-points')[0];
-var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-var animatePoints = function(pointNodes) {
+
+var animatePoints = function() {
     
-    forEach(pointNodes, function(point) {
-        point.style.opacity = 1;
-        point.style.transform = "scaleX(1) translateY(0)";
-        point.style.msTransform = "scaleX(1) translateY(0)";
-        point.style.WebkitTransform = "scaleX(1) translateY(0)";
-    });
+    var revealPoint = function() {
+        $(this).css({
+            opacity:1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    }
+    
+    $.each($(".point"), revealPoint);
 
 };
 
-window.onload = function() {
+$(window).load(function() {
     
-    if(window.innerHeight > 950) {
+    if($(window).height() > 950) {
          animatePoints(pointNodes);
     }
 
-    window.addEventListener('scroll', function(event) {
-        if(document.documentElement.scrollTop || document.body.scrollTop > scrollDistance) {
-             animatePoints(pointNodes);
-        }
+    var scrollDistance = $(".selling-points").offset().top - $(window).height();
+    
+    $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
+        } 
     });
     
     
-}
+});
 
 
